21a8331a03b25c2892b9a9a885befb13f38d411b
Artemis <Artemis.davakou@chasacademy.se>
2026-01-09 14:05:52 +0100

    feat: update NestFactory initialization to specify NestExpressApplication type

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.set('trust proxy', 'loopback');
   // Enable CORS for client
   app.enableCors({
     origin: 'http://localhost:5173',
