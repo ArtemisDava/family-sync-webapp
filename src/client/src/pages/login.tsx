4b167f5a6c486d510d07af78a7bbb1e9c4fe24e8
Artemis <Artemis.davakou@chasacademy.se>
2026-01-09 13:41:56 +0100

    feat: enhance login and signup pages with back navigation and improved styling

--- src/client/src/pages/login.tsx
+++ src/client/src/pages/login.tsx
@@ -6,6 +6,8 @@ import { useUser } from "../contexts/user.context";
 import { type LoginDto } from "../models/login.dto";
 import type LoginInformation from "../models/loginInformation";
 import { Link } from "react-router-dom";
+import { IonIcon } from "@ionic/react";
+import { arrowBack } from "ionicons/icons";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -42,53 +44,69 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="">
-      <form className="" onSubmit={handleSubmit}>
-        <div className="">
-          <label className="">
-            Email
+    <div className="flex items-center justify-center gap-6  min-h-[86vh] p-6 lg:p-0 mx-auto container">
+      <div className="mb-6 bg-logo lg:w-1/2 min-h-[86vh] flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Family Sync!</h1>
+      </div>
+      <div className="lg:w-1/2 p-6">
+        <button
+          className="flex items-center gap-2 mb-12 cursor-pointer opcacity-75 hover:opacity-100"
+          onClick={() => navigate(-1)}
+        >
+          <IonIcon icon={arrowBack} className="p-2 text-" />
+          <span className="text-md font-medium cursor-pointer">Back</span>
+        </button>
+        <h1 className="text-2xl font-bold mb-4">Welcome back!</h1>
+
+        <form className="my-20" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4 mb-4">
+            <label className="" htmlFor="email">
+              Email
+            </label>
             <input
               type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className=""
               required
+              className="border border-black/50 rounded-md p-2"
+              name="email"
             />
-          </label>
-          <label className="">
-            Password
+            <label className="" htmlFor="password">
+              Password
+            </label>
             <input
               type="password"
               value={password}
+              name="password"
               onChange={(e) => setPassword(e.target.value)}
-              className=""
+              className="border border-black/50 rounded-md p-2"
               required
             />
-          </label>
-        </div>
-        {error && <div className="">{error}</div>}
-        <div className="">
-          <Button
-            variant="contained"
-            size="small"
-            color="inherit"
-            type="submit"
-          >
-            Sign in
-          </Button>
-        </div>
-        <div className="">
-          <div className=""></div>
-          <span className="">or</span>
-          <div className=""></div>
-        </div>
-        <div className="">
-          <span>Don't have an account yet?</span>
-          <Link to={"/singup"} className="">
-            Sign up
-          </Link>
-        </div>
-      </form>
+          </div>
+          {error && <div className="">{error}</div>}
+          <div className="">
+            <Button
+              variant="contained"
+              size="small"
+              color="secondary"
+              type="submit"
+              fullWidth={true}
+              sx={{ px: 4, py: 1 }}
+            >
+              Login
+            </Button>
+          </div>
+          <div className="w-full flex flex-col items-center gap-2 mt-4">
+            <p>Don't have an account yet?</p>
+            <Link
+              to={"/singup"}
+              className="w-full border border-black/50 mx-auto text-center rounded-md p-2"
+            >
+              Sign up
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/client/src/pages/signup.tsx
+++ src/client/src/pages/signup.tsx
@@ -8,6 +8,8 @@ import { Link } from "react-router-dom";
 import type { SignUpDto } from "../models/signup.dto";
 import type { SignUpResponseDto } from "../models/signupResponse.dto";
 import { useSearchParams } from "react-router-dom";
+import { IonIcon } from "@ionic/react";
+import { arrowBack } from "ionicons/icons";
 
 export default function SignupPage() {
   const [email, setEmail] = useState("");
@@ -46,68 +48,98 @@ export default function SignupPage() {
   };
 
   return (
-    <form
-      className="flex flex-col w-full max-w-80 mx-auto
-          text-start gap-8 text-base font-normal"
-      onSubmit={handleSubmit}
-    >
-      <div className="flex flex-col gap-4">
-        {invite && (
-          <div className="p-4 bg-green-600 rounded text-white text-center font-bold">
-            You have been invited! Please sign up to join.
-          </div>
-        )}
-        <label className="flex flex-col gap-1 text-base font-bold">
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="rounded px-3 py-2 bg-white/10 font-normal"
-            required
-          />
-        </label>
-        <label className="flex flex-col gap-1 text-base font-bold">
-          Name
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="rounded px-3 py-2 bg-white/10 font-normal"
-            required
-          />
-        </label>
-        <label className="flex flex-col gap-1  text-base font-bold">
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="rounded px-3 bg-white/10 py-2  font-normal"
-            required
-          />
-        </label>
+    <div className="flex items-center justify-center gap-6  min-h-[86vh] p-6 lg:p-0 mx-auto container">
+      <div className="mb-6 bg-logo lg:w-1/2 min-h-[86vh] flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Family Sync!</h1>
       </div>
-      {error && <div className="text-red-400 text-sm text-center">{error}</div>}
-      <div className="flex flex-col gap-4">
-        <Button variant="contained" size="small" color="inherit" type="submit">
-          Sign up
-        </Button>
-      </div>
-      <div className="flex items-center gap-2">
-        <div className="h-px flex-1 bg-white"></div>
-        <span className="text-xs font-bold">or</span>
-        <div className="h-px flex-1 bg-white"></div>
-      </div>
-      <div className="flex flex-col items-center font-bold">
-        <span>Do you have an account?</span>
-        <Link
-          to={"/login"}
-          className="flex cursor-pointer items-center gap-1 text-[#EB634B] transition hover:text-[#6DBE45]"
+      <div className="lg:w-1/2 p-6">
+        <button
+          className="flex items-center gap-2 mb-12 cursor-pointer opcacity-75 hover:opacity-100"
+          onClick={() => navigate(-1)}
         >
-          Sign in
-        </Link>
+          <IonIcon icon={arrowBack} className="p-2 text-" />
+          <span className="text-md font-medium cursor-pointer">Back</span>
+        </button>
+        <h1 className="text-2xl font-bold mb-4">Create an account</h1>
+
+        <form
+          className="flex flex-col w-full my-20
+          text-start gap-8 text-base font-normal"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col gap-4">
+            {invite && (
+              <div className="p-4 bg-green-600 rounded text-white text-center font-bold">
+                You have been invited! Please sign up to join.
+              </div>
+            )}
+            <label
+              className="flex flex-col gap-1 text-base font-bold"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              value={email}
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="px-3 py-2 bg-white/10 font-normal border border-black/50 rounded-md p-2"
+              required
+            />
+            <label
+              className="flex flex-col gap-1 text-base font-bold"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+              className="px-3 py-2 bg-white/10 font-normal border border-black/50 rounded-md p-2"
+              required
+            />
+            <label
+              className="flex flex-col gap-1  text-base font-bold"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="px-3 bg-white/10 py-2  font-normal border border-black/50 rounded-md p-2"
+              required
+            />
+          </div>
+          {error && (
+            <div className="text-red-400 text-sm text-center">{error}</div>
+          )}
+          <div className="flex flex-col gap-4">
+            <Button
+              variant="contained"
+              size="small"
+              color="inherit"
+              type="submit"
+            >
+              Sign up
+            </Button>
+          </div>
+          <div className="w-full flex flex-col items-center gap-2 mt-4">
+            <span>Do you have an account?</span>
+            <Link
+              to={"/login"}
+              className="w-full border border-black/50 mx-auto text-center rounded-md p-2"
+            >
+              Sign in
+            </Link>
+          </div>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
