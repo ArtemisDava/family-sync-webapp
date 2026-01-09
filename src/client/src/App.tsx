a6899c771000aad89e5168e7c39e95fd027010d3
Artemis <Artemis.davakou@chasacademy.se>
2026-01-09 13:47:24 +0100

    feat: implement protected routes for user authentication and enhance routing structure

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -7,24 +7,49 @@ import SignupPage from "./pages/signup";
 import ProfilePage from "./pages/profile";
 import FamiliesPage from "./pages/families";
 import InvitePage from "./pages/invite";
+import CreateAChildPage from "./pages/add-child";
 import ChildPage from "./pages/child";
+import SchedulePage from "./pages/schedule";
+import ScheduleXPage from "./pages/schedule-x";
+import AdminDashboard from "./pages/admin-dashboard";
+import { ModalProvider } from "./contexts/modal.context";
+import { useUser } from "./contexts/user.context";
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/singup" element={<SignupPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/families/:id" element={<FamiliesPage />} />
-          <Route path="/families/:id/add-child" element={<ChildPage />} /> /
-          <Route path="/invite/:code" element={<InvitePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/singup" element={<SignupPage />} />
+            <Route path="/*" element={<ProtectedRoutes />} />
+          </Routes>
+        </BrowserRouter>
+      </ModalProvider>
     </UserProvider>
+  );
+}
+
+function ProtectedRoutes() {
+  const { user } = useUser();
+
+  if (!user) {
+    return <HomePage />;
+  }
+
+  return (
+    <Routes>
+      <Route path="/admin/preview" element={<AdminDashboard />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/schedules" element={<ScheduleXPage />} />
+      <Route path="/families/:id" element={<FamiliesPage />} />
+      <Route path="/child/:id" element={<ChildPage />} />
+      <Route path="/families/:id/add-child" element={<CreateAChildPage />} />
+      <Route path="/invite/:code" element={<InvitePage />} />
+    </Routes>
   );
 }
 
