4caba2ea86feb4abf66ee8da33278777c179a9d3
Artemis <Artemis.davakou@chasacademy.se>
2026-01-11 07:43:14 +0100

    refactor: remove unused routes and clean up imports in App component

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -5,15 +5,12 @@ import { UserProvider } from "./contexts/user.context";
 import LoginPage from "./pages/login";
 import SignupPage from "./pages/signup";
 import ProfilePage from "./pages/profile";
-import FamiliesPage from "./pages/families";
 import InvitePage from "./pages/invite";
-import CreateAChildPage from "./pages/add-child";
-import ChildPage from "./pages/child";
-import SchedulePage from "./pages/schedule";
 import ScheduleXPage from "./pages/schedule-x";
 import AdminDashboard from "./pages/admin-dashboard";
 import { ModalProvider } from "./contexts/modal.context";
 import { useUser } from "./contexts/user.context";
+import AdminDashboardPreview from "./pages/admin-dashboard-preview";
 
 function App() {
   return (
@@ -26,6 +23,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/singup" element={<SignupPage />} />
             <Route path="/*" element={<ProtectedRoutes />} />
+            <Route path="/invite/:code" element={<InvitePage />} />
           </Routes>
         </BrowserRouter>
       </ModalProvider>
@@ -42,13 +40,11 @@ function ProtectedRoutes() {
 
   return (
     <Routes>
-      <Route path="/admin/preview" element={<AdminDashboard />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin/preview" element={<AdminDashboardPreview />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/schedules" element={<ScheduleXPage />} />
-      <Route path="/families/:id" element={<FamiliesPage />} />
-      <Route path="/child/:id" element={<ChildPage />} />
-      <Route path="/families/:id/add-child" element={<CreateAChildPage />} />
-      <Route path="/invite/:code" element={<InvitePage />} />
     </Routes>
   );
 }
