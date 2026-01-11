96a10d52506cbedbab340d9deb19368c6fc38653
Artemis <Artemis.davakou@chasacademy.se>
2026-01-11 07:45:36 +0100

    refactor: remove unused useState import from HomePage component

--- src/client/src/pages/home.tsx
+++ src/client/src/pages/home.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import Hero from "../components/organisms/hero";
 
 function HomePage() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Hero />
