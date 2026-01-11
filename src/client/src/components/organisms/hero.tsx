6478091c85264eef526368ebf8ae3a22cc00ee5e
Artemis <Artemis.davakou@chasacademy.se>
2026-01-11 07:33:55 +0100

    refactor: update Hero component layout and improve button implementation

--- src/client/src/components/organisms/hero.tsx
+++ src/client/src/components/organisms/hero.tsx
@@ -1,21 +1,32 @@
+import Button from "@mui/material/Button";
+
 export default function Hero() {
   return (
-    <section className="bg-logo text-black py-20 h-[80vh] flex items-center">
+    <section className="bg-logo text-black py-20 h-[80vh] flex items-center max-w-[72rem] mx-auto">
       <div className="container mx-auto px-4 text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-6">
+        <h1 className="text-4xl md:text-5xl font-bold mb-6">
           Keep your family in sync — without the stress.{" "}
         </h1>
         <p className="text-lg md:text-2xl mb-8">
-          Family Sync brings all your family’s schedules, reminders, and
+          Family-Sync brings all your family’s schedules, reminders, and
           expenses together in one place. Share what matters, keep the rest
           private, and take back control of your busy life.{" "}
         </p>
-        <a
-          href="#get-started"
-          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full transition duration-300"
+        <Button
+          variant="contained"
+          size="small"
+          color="secondary"
+          type="submit"
+          fullWidth={false}
+          sx={{
+            px: 4,
+            py: 2,
+            textTransform: "none",
+            fontSize: "1rem",
+          }}
         >
           Get Started
-        </a>
+        </Button>
       </div>
     </section>
   );
